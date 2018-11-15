ad60936e276c84a3812b923beecfe0443de52796
Martin Tomov <mtomov@outlook.com>
2018-11-15 12:42:58 +0000

    :art: Lint ECVerify
      - to comply with Solium rules
    
      - Use 2 spaces indentation

--- libraries/ECVerify.sol
+++ libraries/ECVerify.sol
@@ -21,79 +21,90 @@ pragma solidity ^0.4.18;
 // Written by Alex Beregszaszi (@axic), use it under the terms of the MIT license.
 //
 
+
 library ECVerify {
-    // Duplicate Solidity's ecrecover, but catching the CALL return value
-    function safer_ecrecover(bytes32 hash, uint8 v, bytes32 r, bytes32 s) internal returns (bool, address) {
-        // We do our own memory management here. Solidity uses memory offset
-        // 0x40 to store the current end of memory. We write past it (as
-        // writes are memory extensions), but don't update the offset so
-        // Solidity will reuse it. The memory used here is only needed for
-        // this context.
+  // Duplicate Solidity's ecrecover, but catching the CALL return value
+  function saferEcRecover(
+    bytes32 hash,
+    uint8 v,
+    bytes32 r,
+    bytes32 s
+  )
+  internal returns (bool, address)
+  {
+    // We do our own memory management here. Solidity uses memory offset
+    // 0x40 to store the current end of memory. We write past it (as
+    // writes are memory extensions), but don't update the offset so
+    // Solidity will reuse it. The memory used here is only needed for
+    // this context.
 
-        // FIXME: inline assembly can't access return values
-        bool ret;
-        address addr;
+    // FIXME: inline assembly can't access return values
+    bool ret;
+    address addr;
 
-        assembly {
-            let size := mload(0x40)
-            mstore(size, hash)
-            mstore(add(size, 32), v)
-            mstore(add(size, 64), r)
-            mstore(add(size, 96), s)
+    // solium-disable-next-line security/no-inline-assembly
+    assembly {
+      let size := mload(0x40)
+      mstore(size, hash)
+      mstore(add(size, 32), v)
+      mstore(add(size, 64), r)
+      mstore(add(size, 96), s)
 
-            // NOTE: we can reuse the request memory because we deal with
-            //       the return code
-            ret := call(3000, 1, 0, size, 128, size, 32)
-            addr := mload(size)
-        }
-
-        return (ret, addr);
+      // NOTE: we can reuse the request memory because we deal with
+      //       the return code
+      ret := call(3000, 1, 0, size, 128, size, 32)
+      addr := mload(size)
     }
 
-    function ecrecovery(bytes32 hash, bytes sig) public returns (bool, address) {
-        bytes32 r;
-        bytes32 s;
-        uint8 v;
+    return (ret, addr);
+  }
 
-        if (sig.length != 65)
-          return (false, 0);
+  function ecrecovery(bytes32 hash, bytes sig) public returns (bool, address) {
+    bytes32 r;
+    bytes32 s;
+    uint8 v;
 
-        // The signature format is a compact form of:
-        //   {bytes32 r}{bytes32 s}{uint8 v}
-        // Compact means, uint8 is not padded to 32 bytes.
-        assembly {
-            r := mload(add(sig, 32))
-            s := mload(add(sig, 64))
+    if (sig.length != 65)
+      return (false, 0);
 
-            // Here we are loading the last 32 bytes. We exploit the fact that
-            // 'mload' will pad with zeroes if we overread.
-            // There is no 'mload8' to do this, but that would be nicer.
-            v := byte(0, mload(add(sig, 96)))
+    // The signature format is a compact form of:
+    //   {bytes32 r}{bytes32 s}{uint8 v}
+    // Compact means, uint8 is not padded to 32 bytes.
+    // solium-disable-next-line security/no-inline-assembly
+    assembly {
+      r := mload(add(sig, 32))
+      s := mload(add(sig, 64))
 
-            // Alternative solution:
-            // 'byte' is not working due to the Solidity parser, so lets
-            // use the second best option, 'and'
-            // v := and(mload(add(sig, 65)), 255)
-        }
+      // Here we are loading the last 32 bytes. We exploit the fact that
+      // 'mload' will pad with zeroes if we overread.
+      // There is no 'mload8' to do this, but that would be nicer.
+      v := byte(0, mload(add(sig, 96)))
 
-        // albeit non-transactional signatures are not specified by the YP, one would expect it
-        // to match the YP range of [27, 28]
-        //
-        // geth uses [0, 1] and some clients have followed. This might change, see:
-        //  https://github.com/ethereum/go-ethereum/issues/2053
-        if (v < 27)
-          v += 27;
-
-        if (v != 27 && v != 28)
-            return (false, 0);
-
-        return safer_ecrecover(hash, v, r, s);
+      // Alternative solution:
+      // 'byte' is not working due to the Solidity parser, so lets
+      // use the second best option, 'and'
+      // v := and(mload(add(sig, 65)), 255)
     }
 
-    function ecverify(bytes32 hash, bytes sig, address signer) public returns (bool) {
-        bool ret;
-        address addr;
-        (ret, addr) = ecrecovery(hash, sig);
-        return ret == true && addr == signer;
-    }
+    // albeit non-transactional signatures are not specified by the YP, one would expect it
+    // to match the YP range of [27, 28]
+    //
+    // geth uses [0, 1] and some clients have followed. This might change, see:
+    //  https://github.com/ethereum/go-ethereum/issues/2053
+    if (v < 27)
+      v += 27;
+
+    if (v != 27 && v != 28)
+        return (false, 0);
+
+    // solium-disable-next-line arg-overflow
+    return saferEcRecover(hash, v, r, s);
+  }
+
+  function ecverify(bytes32 hash, bytes sig, address signer) public returns (bool) {
+    bool ret;
+    address addr;
+    (ret, addr) = ecrecovery(hash, sig);
+    return ret == true && addr == signer;
+  }
 }
